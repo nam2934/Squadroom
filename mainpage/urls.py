0507763c68020c847067288abb2cb42a19fdadc4
Namjin Jeong <nam2934@naver.com>
2021-02-28 12:09:24 +0000

    make notice list view

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('notice/', views.notice, name='notice'),
+    path('notice/', views.NoticeListView.as_view(), name='notice'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from mainpage.models import Notice
 
 def index(request):
     return render(request, "index.html")
 
 def notice(request):
     return render(request, "notice.html")
+
+class NoticeListView(generic.ListView):
+    model = Notice
+    template_name = "notice.html"
